3b691bc85f255fd8e746034bac0597111965c28d
swapnilkag <swapnilkag2@gmail.com>
2024-10-19 22:43:58 +0530

    env added

--- index.js
+++ index.js
@@ -1,3 +1,61 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const path = require('path');
+// const connectDB = require('./db');
+// const app = express();
+// const corsOptions = {
+//     origin: 'http://localhost:3039', 
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true, 
+// };
+
+// app.use(cors(corsOptions));
+// app.use(express.json()); // For parsing application/json
+// app.use(bodyParser.urlencoded({
+//     extended: false
+// }));
+// /* configure body-parser */// Middleware
+// app.use(bodyParser.json());
+// app.use('/public', express.static(path.join(__dirname, 'public')));
+
+
+// //log requested url
+// app.use((req, res, next) => {
+//     console.log(req.path, req.method);
+//     next();
+// });
+
+// const frontendProductRoute = require('./frontend_route/product_route');
+// const fronted_route = require('./frontend_route/auth_route');
+// app.use('/auth', fronted_route);
+// app.use('/product', frontendProductRoute);
+// app.use('/order', require('./frontend_route/orders_route'));
+
+
+// const product_route = require('./admin_routes/product_route');
+// const admin_route = require('./admin_routes/auth_route');
+// const customer_route = require('./admin_routes/customer_route');
+// app.use('/admin/auth', admin_route);
+// app.use('/admin/product', product_route);
+// app.use('/admin/customer', customer_route);
+// app.use('/admin/category', require('./admin_routes/category_route'))
+
+
+
+
+
+
+// connectDB('E-commerce').then(() => {
+//     console.log('mongoose connected successfully');
+// }).catch((err) => {
+//     console.log('Error connecting to MongoDB:', err);
+// });
+// app.listen(3000, () => {
+//     console.log("Server is listening on port 3000");
+// });
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -5,23 +63,20 @@ const cors = require('cors');
 const path = require('path');
 const connectDB = require('./db');
 const app = express();
+require('dotenv').config();
+
 const corsOptions = {
-    origin: 'http://localhost:3039', // Replace with your frontend origin
+    origin: process.env.CORS_ORIGIN,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    credentials: true,
 };
 
 app.use(cors(corsOptions));
-app.use(express.json()); // For parsing application/json
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-/* configure body-parser */// Middleware
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-
-//log requested url
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
@@ -33,25 +88,20 @@ app.use('/auth', fronted_route);
 app.use('/product', frontendProductRoute);
 app.use('/order', require('./frontend_route/orders_route'));
 
-
 const product_route = require('./admin_routes/product_route');
 const admin_route = require('./admin_routes/auth_route');
 const customer_route = require('./admin_routes/customer_route');
 app.use('/admin/auth', admin_route);
 app.use('/admin/product', product_route);
 app.use('/admin/customer', customer_route);
-app.use('/admin/category', require('./admin_routes/category_route'))
+app.use('/admin/category', require('./admin_routes/category_route'));
 
-
-
-
-
-
-connectDB('E-commerce').then(() => {
+connectDB(process.env.MONGODB_URI).then(() => {
     console.log('mongoose connected successfully');
 }).catch((err) => {
     console.log('Error connecting to MongoDB:', err);
 });
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+
+app.listen(process.env.PORT, process.env.HOST, () => {
+    console.log(`Server is listening on port ${process.env.PORT}`);
 });
